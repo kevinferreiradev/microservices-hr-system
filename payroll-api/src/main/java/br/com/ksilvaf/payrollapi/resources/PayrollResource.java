05c8db210ed9bbbdbeb97a66abe126c79c9548e1
kevinferreiradev <ferreirakevin325@gmail.com>
2022-03-17 01:33:00 -0300

    implementação OpenFeign na comunicação entre APIs

--- payroll-api/src/main/java/br/com/ksilvaf/payrollapi/resources/PayrollResource.java
+++ payroll-api/src/main/java/br/com/ksilvaf/payrollapi/resources/PayrollResource.java
@@ -1,22 +1,29 @@
 package br.com.ksilvaf.payrollapi.resources;
 
 import br.com.ksilvaf.payrollapi.domain.Payroll;
+import br.com.ksilvaf.payrollapi.domain.User;
+import br.com.ksilvaf.payrollapi.feignClients.UserFeign;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value="api/payments")
 public class PayrollResource {
 
+    private final UserFeign userFeign;
+
     @GetMapping(value = "/{workerId}")
     public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment) {
+        User user = userFeign.findById(workerId).getBody();
         return ResponseEntity.ok().body(
                 new Payroll(
-                        "Kevin",
+                        user.getName(),
                         payment.getDescription(),
-                        payment.getHourlyPrice(),
-                        100.0,
-                        payment.getHourlyPrice() * payment.getWorkedHours()
+                        user.getHourlyPrice(),
+                        payment.getWorkedHours(),
+                        user.getHourlyPrice() * payment.getWorkedHours()
                 )
         );
 
