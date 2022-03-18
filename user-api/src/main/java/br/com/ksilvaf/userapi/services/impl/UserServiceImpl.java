6e03412a63c3dddd7e0533994f60030513c1b361
kevinferreiradev <ferreirakevin325@gmail.com>
2022-03-18 08:59:33 -0300

    load balancing

--- user-api/src/main/java/br/com/ksilvaf/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/ksilvaf/userapi/services/impl/UserServiceImpl.java
@@ -5,20 +5,25 @@ import br.com.ksilvaf.userapi.repositories.UserRepository;
 import br.com.ksilvaf.userapi.services.UserService;
 import br.com.ksilvaf.userapi.services.exeptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
 
     private final UserRepository repository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("PAYROLL_SERVICE ::: Get request on" + env.getProperty("local.server.port") + " port");
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
